b6f220a7355004a47a19603fc8adc22dc3d5ff04
32605 <116140798+32605@users.noreply.github.com>
2022-11-29 12:11:06 +0700

    Add files via upload

---
+++ List_element.py
@@ -0,0 +1,8 @@
+goals = ['call to grandma', 'shopping', 'cook dinner']
+print(goals)
+i = input('Which goal do you want to chane? ')
+goals[0] = 'make home work'
+print(goals)
+#i = input('Which goal do you want to print?: ')
+#i = int(i) # convert letter(str) to the number (integer)
+#print(goals[i])
